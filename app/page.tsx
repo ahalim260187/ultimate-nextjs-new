2c79e5b83bf769c60f2b90dbe9f45b859d831997
Abdul Halim <raffahaitami@gmail.com>
2026-05-02 09:17:47 +0700

    Add Global CSS Tailwind Theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <h1 className="text-6xl text-amber-500">Home</h1>;
+  return (
+    <div>
+      <h1 className="h1-bold">Home</h1>
+      <p className="background-light800_dark400">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum architecto soluta incidunt! Dolorem enim,
+        ut voluptate corporis, molestiae atque architecto repellendus nesciunt necessitatibus, vel autem error deleniti
+        expedita. Vel, facere!
+      </p>
+    </div>
+  );
 }
